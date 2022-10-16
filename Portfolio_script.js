2c1e46418f9c6ae347e46f60adc766cc3f2ded4e
Nicholas-Abell <108183070+Nicholas-Abell@users.noreply.github.com>
2022-10-16 13:30:43 -0500

    Update Portfolio_script.js

--- Portfolio_script.js
+++ Portfolio_script.js
@@ -23,4 +23,69 @@ faders.forEach(fader => {
 
 sliders.forEach(slider => {
     appearOnScroll.observe(slider);
-})
+});
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+var isInViewport = function (elem, className) {
+    var bounding = elem.getBoundingClientRect();
+    if (elem.classList.contains(className)) {
+        return (
+            bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+};
+
+//Navbar
+var icons = document.querySelectorAll('.navbar__icon');
+var marker = document.querySelectorAll('.marker');
+var headerIcon = document.getElementById('home-btn');
+var profileIcon = document.getElementById('profile-btn');
+var skillIcon = document.getElementById('skill-btn');
+var projectIcon = document.getElementById('projects-btn');
+var constactIcon = document.getElementById('contact-btn');
+var active = "navbar__icon--active";
+
+window.addEventListener('scroll', function (event) {
+    marker.forEach(marker => {
+        if (isInViewport(marker, "title-card-home")) {
+            icons.forEach(active => active.classList.remove('navbar__icon--active'));
+            headerIcon.classList.add(active);
+        }
+        if (isInViewport(marker, "about-me__title-card")) {
+            icons.forEach(active => active.classList.remove('navbar__icon--active'));
+            profileIcon.classList.add(active);
+        }
+        if (isInViewport(marker, "projects-section__title")) {
+            icons.forEach(active => active.classList.remove('navbar__icon--active'));
+            projectIcon.classList.add(active);
+        }
+        if (isInViewport(marker, "skill-info__title-card")) {
+            icons.forEach(active => active.classList.remove('navbar__icon--active'));
+            skillIcon.classList.add(active);
+        }
+        if (isInViewport(marker, "contact-section__title")) {
+            icons.forEach(active => active.classList.remove('navbar__icon--active'));
+            constactIcon.classList.add(active);
+        }
+    });
+}, false);
+
+function scrollToElementCentered(className) {
+    document.querySelector(className).scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+        inline: 'center'
+    });
+}
+function scrollToElement(className) {
+    document.querySelector(className).scrollIntoView({
+        behavior: 'smooth',
+    });
+}
