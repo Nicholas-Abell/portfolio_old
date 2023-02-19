a0fd22e372c074e9ea559ea7f75b30723afc99f4
Nick Abell <darkforceyoda@gmail.com>
2023-02-19 11:38:20 -0600

    mapped project list, project styled

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -1,23 +1,34 @@
+import React from 'react';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { useContext } from 'react';
 import { AppContext } from '../App';
 
-const Project = () => {
+const Project = ({ title, description, image, switchSide }) => {
 
     const { FontAwesomeIcon } = useContext(AppContext);
 
     return (
         <div className="project">
-            <h1 className='project__title'>Title</h1>
-            <div className='flex'>
+            <h1 className='project__title'>{title}</h1>
+            <div className='flex'
+                style={switchSide ? { flexDirection: 'row-reverse' } : { flexDirection: 'row' }}>
                 <div className="project__techStack">
                     <FontAwesomeIcon icon={faGithub} className="icon" />
                     <FontAwesomeIcon icon={faGithub} className="icon" />
                     <FontAwesomeIcon icon={faGithub} className="icon" />
                     <FontAwesomeIcon icon={faGithub} className="icon" />
                 </div>
-                <div className="fake-img">
-                    <div className='overlay'></div>
+                <div
+                    className="project__img"
+                    style={{
+                        backgroundImage: `url(${image})`,
+                        backgroundPosition: 'center',
+                        backgroundOrigin: 'center',
+                        backgroundSize: 'cover',
+                    }}>
+                    <div className='overlay'>
+                        <p className='overlay__description'>{description}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -1,6 +1,10 @@
 import './Projects.scss';
 import Project from './Project';
 
+import wordle_clone_img from '../assets/Images/wordle_clone.jpeg';
+import resteraunt_landing_page_img from '../assets/Images/Restaurant_Landing_Page.png';
+import testImg from '../assets/Images/florian-olivo-4hbJ-eymZ1o-unsplash.jpg';
+
 const Projects = ({ projectsRef }) => {
 
     //map later
@@ -8,22 +12,42 @@ const Projects = ({ projectsRef }) => {
         {
             id: 1,
             title: 'Wordle Game',
-            description: '',
-            image: ''
+            description: 'A Wordle game made in React',
+            image: wordle_clone_img,
+            switchSide: false,
         },
         {
             id: 2,
-            title: '',
-            description: '',
-            image: ''
+            title: 'Resteraunt Landing Page',
+            description: 'A stitic website made with vanilla HTML, and SCSS',
+            image: resteraunt_landing_page_img,
+            switchSide: true,
+        },
+        {
+            id: 3,
+            title: 'Test',
+            description: 'Test, Make Someting Worthy of employement',
+            image: testImg,
+            switchSide: false,
         }
     ];
 
     return (
         <section className="projects-section" name='projectsScroll'>
             <h1 ref={projectsRef} className="projects-section__title">"Projects"<em>{'/>'}</em></h1>
-            <Project />
-            <Project />
+            {
+                projectList.map((project) => {
+                    return (
+                        <Project
+                            key={project.id}
+                            title={project.title}
+                            description={project.description}
+                            image={project.image}
+                            switchSide={project.switchSide}
+                        />
+                    )
+                })
+            }
         </section>
     )
 }
