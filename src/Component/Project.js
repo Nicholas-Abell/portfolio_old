59e4fc437e96634a3cd9a9e4bcd0da353380ff89
Nick Abell <darkforceyoda@gmail.com>
2023-02-19 01:05:33 -0600

    styles for project section rework

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -1,6 +1,26 @@
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { useContext } from 'react';
+import { AppContext } from '../App';
+
 const Project = () => {
+
+    const { FontAwesomeIcon } = useContext(AppContext);
+
     return (
-        <h1 style={{ color: 'red' }}>Project</h1>
+        <div className="project">
+            <h1 className='project__title'>Title</h1>
+            <div className='flex'>
+                <div className="project__techStack">
+                    <FontAwesomeIcon icon={faGithub} className="icon" />
+                    <FontAwesomeIcon icon={faGithub} className="icon" />
+                    <FontAwesomeIcon icon={faGithub} className="icon" />
+                    <FontAwesomeIcon icon={faGithub} className="icon" />
+                </div>
+                <div className="fake-img">
+                    <div className='overlay'></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -2,9 +2,27 @@ import './Projects.scss';
 import Project from './Project';
 
 const Projects = ({ projectsRef }) => {
+
+    //map later
+    const projectList = [
+        {
+            id: 1,
+            title: 'Wordle Game',
+            description: '',
+            image: ''
+        },
+        {
+            id: 2,
+            title: '',
+            description: '',
+            image: ''
+        }
+    ];
+
     return (
         <section className="projects-section" name='projectsScroll'>
             <h1 ref={projectsRef} className="projects-section__title">"Projects"<em>{'/>'}</em></h1>
+            <Project />
             <Project />
         </section>
     )
